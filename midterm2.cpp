6d77e3c36adf2c258bd38489ae8388a07b6f50a7
Phillip Wagner <m0ssyblue7@gmail.com>
2025-10-19 18:24:12 -0700

    test runs

--- midterm2.cpp
+++ midterm2.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-int main () {
-    cout << "test test" << endl;
+vector<string> loadNames(const string& filename) {
+    ifstream file(filename);
+    vector<string> names;
+    string name;
+    while (getline(file, name)) {
+        if (!name.empty())
+        names.push_back(name);
+    }
+    return names;
+}
+
+int main() {
+    srand(time(0)); //seeds RNG
+
+    vector<string> names = loadNames("names.txt");
+
+    cout << "Total Names: " << names.size() << endl;
+    cout << "Test name: " << names[rand() % names.size()] << endl;
+
     return 0;
 }
